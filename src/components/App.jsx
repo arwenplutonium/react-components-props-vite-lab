d585beb4b4179fd2dc80a5657b5bc4397da6f979
Daniella Achoki <daniellaachoki@gmail.com>
2026-08-04 22:10:52 +0300

    Build blog with Header, About, ArticleList, and Article components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,51 @@ import React from "react";
 import blogData from "../data/blog";
 
 console.log(blogData);
-
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}</h1>
+    </header>
+  );
+}
+function About(props) {
+  return (
+    <aside>
+      <img src={props.image} alt="blog logo" />
+      <p>{props.about}</p>
+    </aside>
+  );
+}
+function ArticleList(props) {
+  return (
+    <main>
+      {props.posts.map((post) => (
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+        />
+      ))}
+    </main>
+  );
+}
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
+  );
+}
+function Article(props) {
+  return (
+    <article>
+      <h3>{props.title}</h3>
+      <small>{props.date}</small>
+      <p>{props.preview}</p>
+    </article>
   );
 }
 
